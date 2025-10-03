00a2e21044b4b42f8d507cc33632d297215a0c2e
Killian <martykillian7@gmail.com>
2025-10-03 23:37:06 +0200

    fix deploy

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -8,7 +8,7 @@ export default function SmoothScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      gestureDirection: "vertical",
+      orientation: "vertical",
       smooth: true,
       smoothTouch: false,
       touchMultiplier: 2,
