929c44d369fe064e4a2bc7a86a72ea43e263e74b
Killian <martykillian7@gmail.com>
2025-10-03 23:39:38 +0200

    fix smooth scroll deployment

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -8,10 +8,9 @@ export default function SmoothScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      smooth: true,
-      smoothTouch: false,
+      wheelMultiplier: 1,
       touchMultiplier: 2,
+      orientation: "vertical",
     });
 
     function raf(time: number) {
